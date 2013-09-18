8e270ae9799a6efaaa507a3d4d59317492be9ea7
Ryan Malone <ryan.malone@blondgorilla.com>
2013-09-18 16:42:01 +0800

    first commit

---
+++ IndexModule.cs
@@ -0,0 +1,30 @@
+﻿namespace bgProjectCalc
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Get["/resourceRate"] = parameters => getResourceRate(Request.Query.resourceType).ToString();
+        }
+
+        double getResourceRate(string resourceType)
+        {
+            switch (resourceType)
+            {
+                case "AccountManager" :
+                    return 110.00;
+                case "Designer" :
+                    return 80.00;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
